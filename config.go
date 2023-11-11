5d3c065f77b825e31e07b92f4d1c7d01839521c6
Cameron <cameron-04@hotmail.com>
2023-11-10 18:22:00 -0600

    add introspective methods on Config

--- config.go
+++ config.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"github.com/CameronHonis/set"
+	"strings"
 )
 
 type LogManagerConfig struct {
@@ -14,4 +15,16 @@ func NewLogManagerConfig() *LogManagerConfig {
 		DecoratorByEnv: make(map[string]func(string) string),
 		MutedEnvs:      *set.EmptySet[string](),
 	}
+}
+
+func (c *LogManagerConfig) IsEnvDecorated(env string) bool {
+	return GetLogManager().Config.DecoratorByEnv[strings.ToUpper(env)] != nil
+}
+
+func (c *LogManagerConfig) GetEnvDecorator(env string) func(string) string {
+	return GetLogManager().Config.DecoratorByEnv[strings.ToUpper(env)]
+}
+
+func (c *LogManagerConfig) IsEnvMuted(env string) bool {
+	return GetLogManager().Config.MutedEnvs.Has(strings.ToUpper(env))
 }
